bd93c5f8bec9c93e4db8884d06c219e35c92ad9b
cyclingbd007 <cyclingbd007@DESKTOP-H8LRD4K>
2017-10-08 20:16:58 +0600

    zubayer 5th commit

---
+++ bes.sql
@@ -0,0 +1,227 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema bes
+--
+
+CREATE DATABASE IF NOT EXISTS bes;
+USE bes;
+
+--
+-- Definition of table `batch`
+--
+
+DROP TABLE IF EXISTS `batch`;
+CREATE TABLE `batch` (
+  `batchId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `courseId` int(10) unsigned NOT NULL,
+  `session` varchar(100) NOT NULL,
+  `batchName` varchar(100) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `teacherId` int(10) unsigned NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `registerDate` date NOT NULL,
+  PRIMARY KEY (`batchId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `batch`
+--
+
+/*!40000 ALTER TABLE `batch` DISABLE KEYS */;
+/*!40000 ALTER TABLE `batch` ENABLE KEYS */;
+
+
+--
+-- Definition of table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `courseId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `courseName` varchar(100) NOT NULL,
+  `courseCode` varchar(45) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `courseDuration` int(10) unsigned NOT NULL,
+  `courseBook` varchar(255) NOT NULL,
+  PRIMARY KEY (`courseId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `course`
+--
+
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+
+
+--
+-- Definition of table `lesson`
+--
+
+DROP TABLE IF EXISTS `lesson`;
+CREATE TABLE `lesson` (
+  `lessonId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lessonTitle` varchar(255) NOT NULL,
+  `lessonDuration` int(10) unsigned NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `courseId` int(10) unsigned NOT NULL,
+  `presentationFile` varchar(255) NOT NULL,
+  `notesFile` varchar(255) NOT NULL,
+  PRIMARY KEY (`lessonId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `lesson`
+--
+
+/*!40000 ALTER TABLE `lesson` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lesson` ENABLE KEYS */;
+
+
+--
+-- Definition of table `offer_subject`
+--
+
+DROP TABLE IF EXISTS `offer_subject`;
+CREATE TABLE `offer_subject` (
+  `osId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `courseId` int(10) unsigned NOT NULL,
+  `batchId` int(10) unsigned NOT NULL,
+  `teacherId` int(10) unsigned NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `hours` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`osId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `offer_subject`
+--
+
+/*!40000 ALTER TABLE `offer_subject` DISABLE KEYS */;
+/*!40000 ALTER TABLE `offer_subject` ENABLE KEYS */;
+
+
+--
+-- Definition of table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `roomId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `roomType` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `seatCapacity` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`roomId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `room`
+--
+
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+
+
+--
+-- Definition of table `topics`
+--
+
+DROP TABLE IF EXISTS `topics`;
+CREATE TABLE `topics` (
+  `topicsId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `topicsTitle` varchar(255) NOT NULL,
+  `courseId` int(10) unsigned NOT NULL,
+  `lessonId` int(10) unsigned NOT NULL,
+  `description` text,
+  `duration` int(10) unsigned NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`topicsId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `topics`
+--
+
+/*!40000 ALTER TABLE `topics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `topics` ENABLE KEYS */;
+
+
+--
+-- Definition of table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `roleId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(45) NOT NULL,
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_role`
+--
+
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+
+
+--
+-- Definition of table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `userId` int(10) unsigned NOT NULL,
+  `userName` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `photo` varchar(225) DEFAULT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `roleId` int(10) unsigned NOT NULL,
+  `registerDate` date NOT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`userId`,`userName`,`email`,`password`,`status`,`photo`,`firstName`,`lastName`,`roleId`,`registerDate`) VALUES 
+ (1,'zubayer','zubayer@gmail.com','1234',0,'','','',0,'0000-00-00'),
+ (2,'admin','admin@gmail.com','1234',1,'','','',0,'0000-00-00'),
+ (3,'uzzol','uzzol@gmail.com','1234',1,'','','',0,'0000-00-00');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
